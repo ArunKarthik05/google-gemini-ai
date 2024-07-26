ce821944e7ae8dc19cb1c541eeda0b7d9b534062
Arun Karthik K <arun-21611@zeroproxy.zohocorp.com>
2024-07-27 00:06:38 +0530

    products api added

--- server/database.js
+++ server/database.js
@@ -1,20 +1,6 @@
 import Client from "pg/lib/client.js";
 import "dotenv/config";
 
-// export const client = new Client({
-//   host:'arun-karthik-9741.8nk.gcp-asia-southeast1.cockroachlabs.cloud',
-//   port: 26257,
-//   username: 'root',
-//   password: 'arunkarthik',
-//   database: 'SalesData',
-//   sslmode : 'allow',
-//   sslrootcert: ''
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-// export const client = new Client(process.env.DATABASE_URL);
-
 export const connection = new Client({
   connectionString: process.env.DB_CONNECTION_STRING,
   ssl: {
